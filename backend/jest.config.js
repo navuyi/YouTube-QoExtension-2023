813a4b8c5fdcfabdf3f1ab35ec9abe5a9cc009f5
navuyi <figlusrafal@gmail.com>
2023-06-06 13:15:14 +0200

    jest setup

---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    //testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"],
+    //moduleDirectories: ['node_modules', '<rootDir>']
+};
