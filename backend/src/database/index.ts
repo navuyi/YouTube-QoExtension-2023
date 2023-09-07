5c1e53eef96893aed7ee1725eb761fc81e364bf4
navuyi <figlusrafal@gmail.com>
2023-09-07 12:26:25 +0200

    dev, production cli, config fix, database project position fix

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,19 +1,20 @@
 import * as process from 'process'
+import * as path from 'path'
 import { DataSource } from 'typeorm'
 import { Experiment } from './entities/Experiment'
 import { DebugData } from './entities/DebugData'
 
 import { Assessment } from './entities/Assessment'
 import { Event } from './entities/Event'
-
+console.log(__dirname)
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   //host: "localhost",
   //port: 5432,
   //username: "test",
   //password: "test",
-  database: process.env.NODE_ENV === 'test' ? './src/database/database.test.sqlite' : './src/database/database.sqlite',
-  synchronize: true, // <-- DO NOT USE true IN PRODUCTION
+  database: process.env.NODE_ENV === 'test' ? path.join(__dirname, 'database.test.sqlite') : path.join(__dirname, 'database.sqlite'),
+  synchronize: false, // <-- DO NOT USE true IN PRODUCTION
   logging: false,
   entities: [Experiment, DebugData, Assessment, Event], // <-- all entities have to be imported
   subscribers: [],
